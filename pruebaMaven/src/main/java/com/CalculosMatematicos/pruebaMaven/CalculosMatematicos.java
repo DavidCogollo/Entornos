90c6a6b8947b138d4b08f8a879abbc7ebddb0a90
[DavidCogollo] <[davidcogolloperez@gmail.com]>
2020-04-25 16:22:59 +0200

    añadimos proyecto y modificamos CalculosMatematicos

---
+++ pruebaMaven/src/main/java/com/CalculosMatematicos/pruebaMaven/CalculosMatematicos.java
@@ -0,0 +1,27 @@
+package com.miprimermaven.pruebaMaven;
+
+public class CalculosMatematicos {
+
+	public static int potencia(int base, int exponente) {
+
+		int resul = 1;
+		if (exponente > 0)
+			resul = base * potencia(base, exponente - 1);
+		return resul;
+	}
+
+	public static boolean esPrimo(int numero) {
+
+		boolean primo = true;
+		int divisor = 3;
+		if(numero !=2 && numero%2==0)
+			primo=false;
+		while(primo && divisor< numero/2) {
+			if(numero % divisor==0)
+				primo = false;
+			divisor+=2;
+		}
+		return primo;
+	}
+
+}

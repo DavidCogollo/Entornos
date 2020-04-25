1f72c861511654cf6b205e01553385e799781ece
[DavidCogollo] <[davidcogolloperez@gmail.com]>
2020-04-25 16:27:21 +0200

    actualizamos calculosMatematicos

--- pruebaMaven/src/main/java/com/CalculosMatematicos/pruebaMaven/CalculosMatematicos.java
+++ pruebaMaven/src/main/java/com/CalculosMatematicos/pruebaMaven/CalculosMatematicos.java
@@ -23,5 +23,15 @@ public class CalculosMatematicos {
 		}
 		return primo;
 	}
+ 	public static ArrayList<Integer> listaDePrimosHasta(int numero){
+ 		ArrayList<Integer> listaDePrimos = new ArrayList<Integer>(); 
+		listaDePrimos.add(1); 
+		listaDePrimos.add(2); 
+		listaDePrimos.add(3);
+ 		for(int i=5; i<=numero; i+=2) { 
+			if(esPrimo(i)) listaDePrimos.add(i); 
+		}
+		return listaDePrimos;} 
+
 
 }
